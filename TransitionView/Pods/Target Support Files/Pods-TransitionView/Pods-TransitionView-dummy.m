a556f20dbcfbba3b60ece485fa3ddb0ac769da8e
paraisolorrayne <lorrayneparaisoc@gmail.com>
2018-02-28 09:30:16 -0300

    Added xcworkspace

---
+++ TransitionView/Pods/Target Support Files/Pods-TransitionView/Pods-TransitionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TransitionView : NSObject
+@end
+@implementation PodsDummy_Pods_TransitionView
+@end
---
+++ TransitionView/TransitionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionView
+//
+//  Created by Lorrayne Paraiso C Flor on 28/02/18.
+//  Copyright © 2018 ZupIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
